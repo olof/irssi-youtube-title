5d39bf5fab07380b2a8623af215010aaa3871a61
Olof Johansson <olof@ethup.se>
2011-11-11 20:52:15 +0100

    Adds Irssi::settings_get_bool to mockup Irssi.pm

--- t/lib/Irssi.pm
+++ t/lib/Irssi.pm
@@ -5,7 +5,14 @@ use base qw/Exporter/;
 our @ISA = qw/Exporter/;
 our @EXPORT = qw/MSGLEVEL_CLIENTCRAP/;
 
+my $settings = { };
+
 sub signal_add { 1 };
 sub settings_add_bool { 1 };
+
+sub settings_get_bool { 
+	my $k = shift;
+	return $settings->{$k}
+}
 
 1;
